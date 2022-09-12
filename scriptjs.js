85471c8249e667566fa25daae352cbce3a4aac81
madhusudhan1304 <113178127+madhusudhan1304@users.noreply.github.com>
2022-09-12 19:27:10 +0530

    Update scriptjs.js

--- scriptjs.js
+++ scriptjs.js
@@ -1,18 +1,18 @@
 function validateAge(today, dobobj) {
-  var age = today.getFullYear() - dobobj.getFullYear();
-  var m = today.getMonth() - dobobj.getMonth();
-  if (m < 0 || (m === 0 && today.getDate() < dobobj.getDate())) {
-    age--;
+  var Age = today.getFullYear() - dobobj.getFullYear();
+  var mo = today.getMonth() - dobobj.getMonth();
+  if (mo < 0 || (mo === 0 && today.getDate() < dobobj.getDate())) {
+    Age--;
   }
-  return age;
+  return Age;
 }
 let dobelement = document.getElementById("dob");
 dobelement.addEventListener("change", () => {
-  let [y, m, d] = document.getElementById("dob").value.split("-");
-  let dob = new Date(y, m, d);
+  let [y, mo, d] = document.getElementById("dob").value.split("-");
+  let dob = new Date(y, mo, d);
   let Today = new Date();
-  age = validateAge(Today, dob);
-  if (age < 18 || age > 55) {
+  Age = validateAge(Today, dob);
+  if (Age < 18 || Age > 55) {
     dobelement.setCustomValidity("age must lie in 18 and 55 years!!!");
 
     return;
